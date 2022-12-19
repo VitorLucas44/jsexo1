f78a32145479417db81904467e3af0779fd09c70
VitorLucas44 <vitoriosolucas@hotmail.com>
2022-12-19 11:17:26 +0100

    exo1

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+let ma_phrase = "J'aime les tartines et j'en mange"
+console.log(ma_phrase)
+let nombre_par_jours = " 4x par jours"
+ma_phrase + nombre_par_jours
+console.log(ma_phrase+nombre_par_jours)
+let troiseme = 5;
+let quatrieme = 35;
+console.log(troiseme + quatrieme)
+console.log(ma_phrase + " " + troiseme)
+let salut = "salut tous le monde"
+console.log(salut)
+let bye = "aurevoir"
+console.log(bye)
+salut = bye
+console.log(salut)
+let quatre = 4
+let six = 6
+console.log(quatre>six)
+console.log(quatre<six)
+console.log(quatre==six)
+console.log(quatre!=six)
+console.log(quatre===six)
+console.log(quatre!==six)
